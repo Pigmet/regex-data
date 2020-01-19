bab69023042c22521c1320ce11a0004091bd100f
Gou Nakamura <nakamura462645@gmail.com>
2020-01-19 19:34:24 +0900

    added several files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to regex-data
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/regex_data/core_test.clj
@@ -0,0 +1,7 @@
+(ns regex-data.core-test
+  (:require [clojure.test :refer :all]
+            [regex-data.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
